5664d08e3565584d486c379941a31fefdae210a8
Doug Anson <doug.anson@arm.com>
2015-06-23 11:46:18 -0500

    updates

--- src/main/java/org/mbed/mds/configurator/MDSConfiguratorApp.java
+++ src/main/java/org/mbed/mds/configurator/MDSConfiguratorApp.java
@@ -22,9 +22,21 @@ import java.util.Properties;
  */
 public class MDSConfiguratorApp 
 {
-    static class MDSConfigurator implements HttpHandler {
+    static class MDSConfigurator implements HttpHandler {   
+        private Properties m_mds_config_properties;
+        private Properties m_mds_creds_properties;
+        private Properties m_http_coap_media_types_properties;
+        private Properties m_logging_properties;
+        
+        private Properties m_mds_config_properties_updated;
+        
         public MDSConfigurator() {
             super();
+            this.m_mds_config_properties = new Properties();
+            this.m_mds_creds_properties = new Properties();
+            this.m_http_coap_media_types_properties = new Properties();
+            this.m_logging_properties = new Properties();
+            this.m_mds_config_properties_updated = new Properties();
         }
         
         private String fileToString(String filename)  {
@@ -34,7 +46,6 @@ public class MDSConfiguratorApp
             try {
                 String current = new java.io.File( "." ).getCanonicalPath();
                 String fq_filename = current + "/"  + filename;
-                System.out.println("fileToString: Reading File: "+ fq_filename);
                 input = new FileInputStream(fq_filename);
                 Reader reader = new BufferedReader(new InputStreamReader(input));
                 StringBuilder builder = new StringBuilder();
@@ -51,38 +62,24 @@ public class MDSConfiguratorApp
             return null;
         }
         
-        private String initializeTable() {
-            String table = "";
-            
+        private String initializeResponse(String html) {
             // initialize the table with the CSS first
-            table += this.fileToString("css.html");
+            html += this.fileToString("css.html");
             
             // add scripts
-            table += this.fileToString("scripts.html");
+            html += this.fileToString("scripts.html");
             
             // add the table template
-            table += this.fileToString("editor.html");
+            html += this.fileToString("editor.html");
             
             // update some of the key variables
             String dir = this.getWorkingDirectory();
-            table = table.replace("__TITLE__","mDS Configuration Editor v1.0");
+            html = html.replace("__TITLE__","mDS Configuration Editor v1.0");
             
-            // return the table
-            return table;
+            // return the html
+            return html;
         }
-        
-        // open the mDS device server properties
-        private Properties getDeviceServerProperties() { return this.getProperties("deviceserver.properties"); }
-        
-        // open the mDS device server credential properties
-        private Properties getDeviceServerCredentialProperties() { return this.getProperties("credentials.properties"); }
-        
-        // open the mDS device server logging properties
-        private Properties getDeviceServerLoggingProperties() { return this.getProperties("log4j.properties"); }
-        
-        // open the mDS device server medai types properties
-        private Properties getDeviceServerMediaTypesProperties() { return this.getProperties("http-coap-mediatypes.properties"); }
-        
+                
         private String getWorkingDirectory() {
             try {
                 return new java.io.File( "." ).getCanonicalPath();
@@ -92,12 +89,11 @@ public class MDSConfiguratorApp
             return "./";
         }
         // open the mDS properties
-        private Properties getProperties(String filename) {
-            Properties prop = new Properties();
+        private Properties getProperties(Properties prop,String filename) {
             try {
+                prop.clear();
                 String current = this.getWorkingDirectory();
                 String fq_filename = current + "/"  + filename;
-                System.out.println("Reading Properties File: "+ fq_filename);
                 InputStream input = new FileInputStream(fq_filename);
                 prop.load(input);
                 input.close();
@@ -108,7 +104,7 @@ public class MDSConfiguratorApp
             return prop;
         }
         // build out the properties table
-        private String createMDSConfigurationTableAsHTML(Properties props) {
+        private String createConfigTableAsHTML(Properties props,String file) {
             // start the table
             String table = "<table border=\"0\">";
             
@@ -125,7 +121,7 @@ public class MDSConfiguratorApp
                 String height = "auto";
                 String value = props.getProperty(key);               
                 table += "<td id=\"" + key + "\" contenteditable=\"true\" align=\"left\" height=\"" + height + "\" width=\"" + "auto" + "\">" + value + "</td>";
-                String save_button = "<button name=\"save_button\" value=\"" + key + "\" type=\"button\" onclick=saveData('" + key + "') style=\"height:35px;width:80px\">SAVE</button>";
+                String save_button = "<button name=\"save_button\" value=\"" + key + "\" type=\"button\" onclick=saveData('" + key + "','" + file + "') style=\"height:35px;width:80px\">SAVE</button>";
                 table += "<td align=\"center\" height=\"35px\" width=\"210px\">" + save_button + "</td>";
      
                 // finish row
@@ -139,18 +135,15 @@ public class MDSConfiguratorApp
             return table;
         }
         
-        private String buildConfigurationTable(Properties props) {
-            // initialize the table
-            String table = this.initializeTable();
-            
+        private String buildConfigurationTable(String html,Properties props,String file,String key) {            
             // create the actual configuration table has HTML
-            String preference_table = this.createMDSConfigurationTableAsHTML(props);
+            String preference_table = this.createConfigTableAsHTML(props,file);
             
             // fill in the body
-            table = table.replace("__PREFERENCES_TABLE__",preference_table);
+            html = html.replace(key,preference_table);
             
             // return the table
-            return table;
+            return html;
         }
         
         public Map<String, String> queryToMap(String query){
@@ -167,28 +160,158 @@ public class MDSConfiguratorApp
             }
             return result;
         }
+                
+        private String displayDeviceServerProperties(String html) {
+            Properties props = this.getProperties(this.m_mds_config_properties,"deviceserver.properties");
+            return this.buildConfigurationTable(html,props,"deviceserver.properties","__DS_CONFIG_TABLE__");
+        }
+        
+        private String displayDeviceServerCredentials(String html) {
+            Properties props = this.getProperties(this.m_mds_creds_properties,"credentials.properties");
+            return this.buildConfigurationTable(html,props,"credentials.properties","__DS_CREDS_TABLE__");
+        }
+        
+        private String displayCoAPMediaTypesConfig(String html) {
+            Properties props = this.getProperties(this.m_http_coap_media_types_properties,"http-coap-mediatypes.properties");
+            return this.buildConfigurationTable(html,props,"http-coap-mediatypes.properties","__COAP_MEDIA_TYPE_TABLE__");
+        }
+        
+        private String displayLoggingConfig(String html) {
+            Properties props = this.getProperties(this.m_logging_properties,"log4j.properties");
+            return this.buildConfigurationTable(html,props,"log4j.properties","__LOGGING_CONFIG_TABLE__");
+        }
+        
+        // update mDS configuration
+        private void updateDeviceServerConfiguration(String key,String value,String file) {
+            // save the updated value the preferences
+            this.m_mds_config_properties_updated.put(key, value);
+
+            // DEBUG
+            System.out.println("mDS Configuration: Updating " + key + " = " + value);
+        }
+        
+        // update mDS credentials
+        private void updateDeviceServerCredentials(String key,String value,String file) {
+            // save the updated value the preferences
+            this.m_mds_creds_properties.put(key, value);
+
+            // DEBUG
+            System.out.println("mDS Credentials: Updating " + key + " = " + value);
+        }
+        
+        // update HTTP CoAP Media Types
+        private void updateHTTPCoAPMediaTypes(String key,String value,String file) {
+            // save the updated value the preferences
+            this.m_http_coap_media_types_properties.put(key, value);
+
+            // DEBUG
+            System.out.println("HTTP CoAP Media Types: Updating " + key + " = " + value);
+        }
+        
+        // update Logging Configuration
+        private void updateLoggingConfiguration(String key,String value,String file) {
+            // save the updated value the preferences
+            this.m_logging_properties.put(key, value);
+
+            // DEBUG
+            System.out.println("Logging Configuration: Updating " + key + " = " + value);
+        }
+        
+        private void saveDeviceServerConfigurationFile() {
+            // DEBUG
+            System.out.println("Saving mDS Configuration File...");
+            
+            // clear out the updates... 
+            this.m_mds_config_properties_updated.clear();
+        }
+        
+        private void saveDeviceServerCredentialsFile() {
+            // DEBUG
+            System.out.println("Saving mDS Credentials File...");
+        }
+        
+        private void saveHTTPCoAPMediaTypesConfigFile() {
+            // DEBUG
+            System.out.println("Saving HTTP CoAP Media Types Config File...");
+        }
+        
+        private void saveLoggingConfigFile() {
+            // DEBUG
+            System.out.println("Saving Logging Config File...");
+        }
         
         @Override
         public void handle(HttpExchange t) throws IOException {
-            // configuration page request (GET)
-            Properties props = this.getDeviceServerProperties();
+            String html = "";
+            
+            // convert the query string...
             Map<String,String> query = this.queryToMap(t.getRequestURI().getQuery());
+            
+            // update individual settings for a given configuration...
             if (query.get("updated_key") != null) {
-                // save the updated value the preferences
-                String key = query.get("updated_key");
-                String value = query.get("updated_value");
-                props.put(key, value);
-                // DEBUG
-                System.out.println("Updating " + key + " = " + value);
+                String file = query.get("file"); 
+                
+                // mDS Configuration
+                if (file.equalsIgnoreCase("deviceserver.properties")) {
+                    this.updateDeviceServerConfiguration(query.get("updated_key"), query.get("updated_value"), file);
+                }
+                
+                // mDS Credentials
+                if (file.equalsIgnoreCase("credentials.properties")) {
+                    this.updateDeviceServerCredentials(query.get("updated_key"), query.get("updated_value"), file);
+                }
+                
+                // HTTP CoAP Media Types
+                if (file.equalsIgnoreCase("http-coap-mediatypes.properties")) {
+                    this.updateHTTPCoAPMediaTypes(query.get("updated_key"), query.get("updated_value"), file);
+                }
+                
+                // Logging Configuration
+                if (file.equalsIgnoreCase("log4j.properties")) {
+                    this.updateLoggingConfiguration(query.get("updated_key"), query.get("updated_value"), file);
+                }
             }
+            
+            // save configurations...
             if (query.get("save") != null) {
-                // DEBUG
-                System.out.println("Saving Configuration File...");
+                String value = query.get("save");
+                
+                // mDS Configuration
+                if (value.equalsIgnoreCase("ds")) {
+                    this.saveDeviceServerConfigurationFile();
+                }
+                
+                // mDS Credentials
+                if (value.equalsIgnoreCase("creds")) {
+                    this.saveDeviceServerCredentialsFile();
+                }
+                
+                // HTTP CoAP Media Types
+                if (value.equalsIgnoreCase("media")) {
+                    this.saveHTTPCoAPMediaTypesConfigFile();
+                }
+                
+                // Logging Configuration
+                if (value.equalsIgnoreCase("logging")) {
+                    this.saveLoggingConfigFile();
+                }
             }
-            String response = this.buildConfigurationTable(props);
-            t.sendResponseHeaders(200,response.length());
+            
+            // initialize the response
+            html = this.initializeResponse(html);
+
+            // build out the display response
+            html = this.displayDeviceServerProperties(html);
+            html = this.displayDeviceServerCredentials(html);
+            html = this.displayCoAPMediaTypesConfig(html);
+            html = this.displayLoggingConfig(html);
+            
+            // send the response and close
+            t.sendResponseHeaders(200,html.length());
             OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
+            os.write(html.getBytes());
+            
+            // clean up
             os.close();
         }
     }
