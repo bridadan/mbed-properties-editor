f686653c13b6116473972d3c0ad20b6f5d0a08a4
Doug Anson <doug.anson@arm.com>
2016-05-06 18:29:30 -0500

    added support for setting AWS IoT CLI credentials

--- src/main/java/org/mbed/mds/configurator/MDSConfiguratorApp.java
+++ src/main/java/org/mbed/mds/configurator/MDSConfiguratorApp.java
@@ -667,12 +667,20 @@ public class MDSConfiguratorApp
             
             // restart mDS
             if (query.get("mds") != null) {
+                // also reset AWS IoT account creds 
+                this.updateAWSCreds();
+                
+                // then restart the mDS server
                 System.out.println("Restarting mbed Device Server...");
                 this.executeScript("restartMDS.sh");
             }
             
             // restart Connector Bridge
             if (query.get("connectorbridge") != null) {
+                // reset AWS IoT account creds 
+                this.updateAWSCreds();
+                
+                // then restart the bridge
                 System.out.println("Restarting Connector Bridge...");
                 this.executeScript("restartConnectorBridge.sh");
             }
@@ -698,6 +706,22 @@ public class MDSConfiguratorApp
             
             // clean up
             os.close();
+        }
+        
+        // conditionally check and update the AWS IoT CLI creds - NOTE: sensitive to changes in the configuration file!
+        private void updateAWSCreds() {
+            String region = (String)this.m_configurator_properties.get("aws_iot_region");
+            String key_id = (String)this.m_configurator_properties.get("aws_iot_access_key_id");
+            String access_key = (String)this.m_configurator_properties.get("aws_iot_secret_access_key");
+            
+            if (region != null && region.length() > 0 && region.equalsIgnoreCase("AWS_region_goes_here") == false &&
+                key_id != null && key_id.length() > 0 && key_id.equalsIgnoreCase("AWS_Access_Key_ID_goes_here") == false && 
+                access_key != null && access_key.length() > 0 && access_key.equalsIgnoreCase("AWS_Secret_Access_Key_goes_here") == false) {
+                
+                // set the AWS CLI creds...
+                String args = region + " " + key_id + " " + access_key;
+                this.executeScript("set_aws_creds.sh " + args);
+            }
         }
         
         /**
