ffe48eaac81d4f9ad0591e73a33a78b17397907f
Doug Anson <doug.anson@arm.com>
2016-05-06 22:53:54 -0500

    minor fix

--- src/main/java/org/mbed/mds/configurator/MDSConfiguratorApp.java
+++ src/main/java/org/mbed/mds/configurator/MDSConfiguratorApp.java
@@ -710,9 +710,12 @@ public class MDSConfiguratorApp
         
         // conditionally check and update the AWS IoT CLI creds - NOTE: sensitive to changes in the configuration file!
         private void updateAWSCreds() {
-            String region = (String)this.m_mds_config_properties_updated.get("aws_iot_region");
-            String key_id = (String)this.m_mds_config_properties_updated.get("aws_iot_access_key_id");
-            String access_key = (String)this.m_mds_config_properties_updated.get("aws_iot_secret_access_key");
+            String region = (String)this.m_cb_properties.get("aws_iot_region");
+            String key_id = (String)this.m_cb_properties.get("aws_iot_access_key_id");
+            String access_key = (String)this.m_cb_properties.get("aws_iot_secret_access_key");
+            
+            // DEBUG
+            System.out.println("updateAWSCreds: region: " + region + " key_id: " + key_id + " access_key: " + access_key);
             
             if (region != null && region.length() > 0 && region.equalsIgnoreCase("AWS_region_goes_here") == false &&
                 key_id != null && key_id.length() > 0 && key_id.equalsIgnoreCase("AWS_Access_Key_ID_goes_here") == false && 
@@ -720,6 +723,11 @@ public class MDSConfiguratorApp
                 
                 // set the AWS CLI creds...
                 String args = region + " " + key_id + " " + access_key;
+                
+                // DEBUG
+                System.out.println("updateAWSCreds: calling: set_aws_creds.sh " + args);
+                
+                // execute
                 this.executeScript("set_aws_creds.sh " + args);
             }
         }
