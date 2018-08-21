b244d98ab665095b565ba9bada396e6dc63b6915
Doug Anson <doug.anson@arm.com>
2018-08-21 15:52:25 -0500

    updates

--- scripts/restartPropertiesEditor.sh
+++ scripts/restartPropertiesEditor.sh
@@ -2,5 +2,6 @@
 
 cd ${HOME}/properties-editor/scripts
 ./killPropertiesEditor.sh
+sleep 8
 cd ${HOME}/properties-editor
 ./runPropertiesEditor.sh &
--- src/main/java/com/arm/mbed/properties/editor/processor/PropertiesEditorProcessor.java
+++ src/main/java/com/arm/mbed/properties/editor/processor/PropertiesEditorProcessor.java
@@ -98,8 +98,14 @@ public class PropertiesEditorProcessor extends PropertiesEditor implements HttpH
         
         // for now... just make this static
         this.m_key_map.put("api_key","Pelion API Key");
+        this.m_key_map.put("mds_api_token","Pelion API Key");
         this.m_key_map.put("mds_enable_long_poll","Enable Bridge long Polling");
         this.m_key_map.put("api_endpoint_address","Pelion API Address");
+        this.m_key_map.put("mds_address","Pelion API Address");
+        this.m_key_map.put("mds_gw_port","Bridge Webhook Port");
+        this.m_key_map.put("mds_bridge_error_level","Bridge Debug Level");
+        this.m_key_map.put("mds_remove_on_deregistration","Remove Device on De-registration");
+        this.m_key_map.put("iotf_legacy_bridge","Watson IoT Enable Legacy Mode");
         this.m_key_map.put("iotf_api_key","Watson IoT API Key");
         this.m_key_map.put("iotf_auth_token","Watson IoT Authentication Token");
         this.m_key_map.put("threads_core_pool_size","Bridge Threading Pool Size");
@@ -110,6 +116,7 @@ public class PropertiesEditorProcessor extends PropertiesEditor implements HttpH
         this.m_key_map.put("aws_iot_secret_access_key","AWS Access Key Secret");
         this.m_key_map.put("google_cloud_auth_json","Google Cloud Auth JSON");
         this.m_key_map.put("google_cloud_region","Google Cloud Region");
+        this.m_key_map.put("google_cloud_mqtt_port","Google MQTT Port");
         this.m_key_map.put("iot_event_hub_name","Microsoft IoTHub Name");
         this.m_key_map.put("iot_event_hub_sas_token","Microsoft IoTHub SAS Token (iothubowner)");
         this.m_key_map.put("mqtt_address","MQTT Broker IP Address");
